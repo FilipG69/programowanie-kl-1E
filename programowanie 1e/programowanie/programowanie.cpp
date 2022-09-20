c9b10b55f4726b28d9ac9007c35a477ac7d4216e
GAP <GAP>
2022-09-20 08:54:01 +0200

    koniec

---
+++ programowanie 1e/programowanie/programowanie.cpp
@@ -0,0 +1,9 @@
+// programowanie.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "C anada\nU SA\nM exsico\n";
+}
