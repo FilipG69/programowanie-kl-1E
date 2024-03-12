fbefd8e7d6e14a937694e54a24583f2e01419db0
FilipG69 <113014060+FilipG69@users.noreply.github.com>
2024-03-12 21:13:45 +0100

    Add files via upload

---
+++ programowanie 1e/FirstObjectTaskConsoleApp.cpp
@@ -0,0 +1,101 @@
+﻿#include <iostream>
+
+/*
+Zdefiniuj klase opisujący punkt na przestrzeni kartezjańskiej. Możliwości klasy:
+* konstruktory
+* metoda która zwórci odległość tego punktu od poczatku układu współrzednych
+* metoda która zwróci odległość od innego punktu
+* metoda która zwróci informację w której ćwiartce układu współrzednych się znajduje punkt
+* metodę która pokaże informacje o punkcie.
+
+Napisz program który zaprezentuje możliwości obiektu na podstawie tej klasy.
+
+*/
+
+/*
+Zdefiniuj klase opisujące konto bankowe. Możliwości klasy:
+* konstruktory
+* metoda która dokona wpłaty na konto
+* metoda która wypłaty z konta
+* metoda która wykona przelew na inne konto
+* metodę która pokaże informacje o koncie.
+
+Napisz program który zaprezentuje możliwości obiektu na podstawie tej klasy.
+*/
+
+class Point2D
+{
+private:
+	double x;
+	double y;
+	double quarter;
+public:
+	Point2D()
+	{
+		x = 10;
+		y = 6;
+	}
+
+	Point2D(double xx)
+	{
+		x = xx;
+		y = 6;
+	}
+
+	Point2D(double xx, double yy)
+	{
+		x = xx;
+		y = yy;
+	}
+
+	double DistanceFromCenter()
+	{
+		double distance = sqrt(x * x + y * y);
+		return distance;
+	}
+
+	double DiastanceBetweenPoints(Point2D secondPoint)
+	{
+		double distance = sqrt((x - secondPoint.x) * (x - secondPoint.x) + (y - secondPoint.y) * (y - secondPoint.y));
+		return distance;
+	}
+
+	double wichQuarter(Point2D secondPoint)
+	{
+		if (x < 0 && y > 0 )
+		{
+			return quarter = 1;
+		}
+		if (x > 0 && y > 0)
+		{
+			return quarter = 2;
+		}
+		if (x > 0 && y < 0)
+		{
+			return quarter = 3;
+		}
+		if (x < 0 && y < 0)
+		{
+			return quarter = 4;
+		}
+
+	}
+	double pointInformation(Point2D secondPoint)
+	{
+		
+	}
+};
+
+int main()
+{
+	setlocale(LC_CTYPE, "polish");
+	Point2D firstPoint2D;
+	std::cout << "Odległość od środka to " << firstPoint2D.DistanceFromCenter() << "\n";
+	Point2D secondPoint2D(-2, -8.5);
+
+	std::cout << "Odległość pomiędzy dwoma punktami " << secondPoint2D.DiastanceBetweenPoints(firstPoint2D) << "\n";
+	std::cout << "Cwiarka:" << secondPoint2D.wichQuarter(secondPoint2D) << "\n";
+}
+
+//"Zawsze programuj jak gdyby osoba zajmująca się twoim kodem w przyszłości była agresywnym psychopatą,
+//który wie gdzie mieszkasz."
